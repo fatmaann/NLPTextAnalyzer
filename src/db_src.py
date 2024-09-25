7be20a478ce4dd7d6d9f5acd6fd1d1c35e494e4d
fatmaann <sol1222183@gmail.com>
2024-09-25 11:51:28 +0300

    chore sqldbtest

--- src/db_src.py
+++ src/db_src.py
@@ -1,6 +1,6 @@
 import os
 from sqlalchemy import create_engine, Column, Integer, String, ForeignKey, DateTime, func
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import sessionmaker, relationship
 import bcrypt
 
